2d4cb8b96430a30a15863faa8d18dc7188b92aee
shahp7575 <shahp7575@gmail.com>
2024-10-01 13:43:48 -0400

    sh file

--- run_scraper.sh
+++ run_scraper.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-source ~/opt/anaconda3/etc/profile.d/conda.sh  # Adjust this to the location of your Conda installation
+source ~/opt/anaconda3/etc/profile.d/conda.sh
 conda activate coffee_scraper
 python3 src/scraper.py
